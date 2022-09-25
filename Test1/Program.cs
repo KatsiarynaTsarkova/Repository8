7d54986613ebcd35b1c02fb0f877bb9b6ea9390d
Kate <katya_tsarkov@mail.ru>
2022-09-25 21:52:05 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером мхn заполненный случайными 
+//вещественными числами m=3; n=4.
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+double[,] matrix = new double[3, 4];
+Random random = new Random();
+void PrintArray( double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+          Console.Write($"{matr[i, j]} "); 
+         
+        }
+         Console.WriteLine();
+    }
+   
+}
+void FillArray( double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        matr[i,j]=Convert.ToDouble(random.Next(-100,100)/10.0);
+      }  
+    }
+}
+FillArray( matrix);
+Console.WriteLine();
+PrintArray( matrix);
+
